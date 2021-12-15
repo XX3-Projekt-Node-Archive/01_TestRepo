2e3e99879abcdaff4ea2f9409b19d10fbe222de6
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-12-15 09:44:03 +0000

    updates

---
+++ index.js
@@ -0,0 +1,11 @@
+// Das ist mein Workspace!
+
+// Hallo Ich xD
+// Hallo hier ist der Geist der vergangenen Weihnacht, das ist schön!
+
+function start(params) {
+    return "Ich bin der " + params +"!"
+    
+}
+
+console.log(start("Dieter"));
